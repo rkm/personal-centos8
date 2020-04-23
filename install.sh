25d301509c8e93416e8d389b2eb33cfb9e6c3753
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-04-23 20:29:22 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo yum -y install epel-release && \
 sudo yum -y install ansible && \
 curl -L https://api.github.com/repos/rkm/personal-centos8/tarball > repo.tgz && \
 tar xzf repo.tgz && \
-cd rkm-personal-centos8* && \
+pushd rkm-personal-centos8-* && \
 ansible-galaxy install -r requirements.yml && \
-ansible-playbook [-v] [--tags <tags>] local.yml
+ansible-playbook [-v] [--tags <tags>] local.yml && \
+popd && \
+rm -r rkm-personal-centos8*
