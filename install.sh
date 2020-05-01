e20e633756973c461bf9c62a795d85129d5ce55a
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-05-01 17:39:08 +0100

    Add sudo password prompt for initial install

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ curl -Ls https://api.github.com/repos/rkm/personal-centos8/tarball > repo.tgz &&
 tar xzf repo.tgz && \
 pushd rkm-personal-centos8-* && \
 ansible-galaxy install -r requirements.yml && \
-ansible-playbook local.yml && \
+ansible-playbook -K local.yml && \
 popd && \
 rm -r rkm-personal-centos8* repo.tgz
