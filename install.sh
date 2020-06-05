ea4e2c0031a2e05f0f1386d5a98c8887689fe50c
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-06-05 21:37:03 +0000

    refactor install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+PKG=dnf
+OS=8
+
 if [ "$(stat ~/.ssh/id_rsa* | grep 0400 | wc -l)" != "2" ]; then
     echo Install ssh keys with correct permissions first!
     exit 1
 fi
 
-sudo dnf -y update && \
-sudo dnf -y install epel-release && \
-sudo dnf -y install ansible tar && \
-curl -Ls https://api.github.com/repos/rkm/personal-centos8/tarball > repo.tgz && \
+sudo $PKG -y update && \
+sudo $PKG -y install epel-release && \
+sudo $PKG -y install ansible tar && \
+curl -Ls https://api.github.com/repos/rkm/personal-centos${OS}/tarball > repo.tgz && \
 tar xzf repo.tgz && \
-pushd rkm-personal-centos8-* && \
+pushd rkm-personal-centos${OS}-* && \
 ansible-galaxy install -r requirements.yml && \
 ansible-playbook -K local.yml && \
 popd && \
-rm -r rkm-personal-centos8* repo.tgz && \
+rm -r rkm-personal-centos${OS}* repo.tgz && \
 echo "Initial install complete - A reboot is recommended"
